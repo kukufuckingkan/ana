c6303b3e1698f140ebe5b4e8e3e23fb5463b0a15
kukufuckingkan <keitakukukhan@gmail.com>
2024-09-28 03:26:36 +0000

    dsdsd

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="moril",
-    version="0.0.2",
+    version="0.0.1",
     description='A short description of your package',
     author='Ana Keita',
     author_email='keita.kukukhan@gmail.com',    
